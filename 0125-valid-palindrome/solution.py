a0e2e048b31034bfcbb93508d537cc22116366f9
piorus <piotr@rusin.work>
2025-02-07 12:47:03 +0100

    0125 valid palindrome (easy)

---
+++ 0125-valid-palindrome/solution.py
@@ -0,0 +1,27 @@
+"""
+Problem: 125. Valid Palindrome
+Difficulty: Easy
+Concepts: Two Pointers
+Link: https://leetcode.com/problems/valid-palindrome/
+================
+Time complexity:
+Space complexity:
+"""
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            while not s[left].isalnum() and left < right:
+                left += 1
+            while not s[right].isalnum() and left < right:
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
